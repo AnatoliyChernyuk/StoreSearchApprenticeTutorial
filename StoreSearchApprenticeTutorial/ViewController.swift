d7e2095c40ea1e25a8bcfd2880cf7ad36477f1da
Anatoliy Chernyuk <toliatche@gmail.com>
2017-11-27 00:05:00 +0200

    Initial Commit

---
+++ StoreSearchApprenticeTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreSearchApprenticeTutorial
+//
+//  Created by Anatoliy Chernyuk on 11/27/17.
+//  Copyright © 2017 Anatoliy Chernyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

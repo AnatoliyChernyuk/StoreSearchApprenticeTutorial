42cfe978316d1408d21c699fb4d123dc7baf5aa0
Anatoliy Chernyuk <toliatche@gmail.com>
2017-12-01 23:06:06 +0200

    Segmented controller to choose between the categories added

--- StoreSearchApprenticeTutorial/SearchViewController+UISearchBarDelegate.swift
+++ StoreSearchApprenticeTutorial/SearchViewController+UISearchBarDelegate.swift
@@ -10,45 +10,7 @@ import UIKit
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        if !searchBar.text!.isEmpty {
-            searchBar.resignFirstResponder()
-            isLoading = true
-            dataTask?.cancel()
-            tableView.reloadData()
-            
-            searchResults = []
-            hasSearched = true
-            
-            let url = iTunesURL(searchText: searchBar.text!)
-            let session = URLSession.shared
-            dataTask = session.dataTask(with: url, completionHandler: { (data, response, error) in
-                //print("On the main thread? " + (Thread.current.isMainThread ? "Yes" : "No"))
-                if let error = error as NSError?, error.code == -999 {
-                    return //Seaarch was cancelled
-                } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
-                    if let data = data, let jsonDictionary = self.parse(json: data) {
-                        self.searchResults = self.parse(dictionary: jsonDictionary)
-                        self.searchResults.sort(by: <)
-                        DispatchQueue.main.async {
-                            self.isLoading = false
-                            self.tableView.reloadData()
-                        }
-                        return
-                    }
-                    
-                    
-                } else {
-                    print("Failure! \(response!)")
-                }
-                DispatchQueue.main.async {
-                    self.hasSearched = false
-                    self.isLoading = false
-                    self.tableView.reloadData()
-                    self.showNetworkError()
-                }
-            })
-            dataTask?.resume()
-        }
+        performSearch()
     }
     
     func position(for bar: UIBarPositioning) -> UIBarPosition {
--- StoreSearchApprenticeTutorial/SearchViewController.swift
+++ StoreSearchApprenticeTutorial/SearchViewController.swift
@@ -17,6 +17,7 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     var dataTask: URLSessionDataTask?
     var hasSearched = false
@@ -25,7 +26,7 @@ class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = UIEdgeInsets(top: 108, left: 0, bottom: 0, right: 0)
         var cellNib = UINib(nibName: TableViewCellIdentifiers.searchResultCell, bundle: nil)
         tableView.register(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.searchResultCell)
         tableView.rowHeight = 80
@@ -43,14 +44,61 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func iTunesURL(searchText: String) -> URL {
+    func iTunesURL(searchText: String, category: Int) -> URL {
+        let entityName: String
+        switch category {
+        case 1: entityName = "musicTrack"
+        case 2: entityName = "software"
+        case 3: entityName = "ebook"
+        default: entityName = ""
+        }
         let escapedSearchText = searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-        let urlString = String(format: "https://itunes.apple.com/search?term=%@&limit=200", escapedSearchText)
+        let urlString = String(format: "https://itunes.apple.com/search?term=%@&limit=200&entity=%@", escapedSearchText, entityName)
         let url = URL(string: urlString)
         return url!
     }
     
-    
+    func performSearch() {
+        if !searchBar.text!.isEmpty {
+            searchBar.resignFirstResponder()
+            isLoading = true
+            dataTask?.cancel()
+            tableView.reloadData()
+            
+            searchResults = []
+            hasSearched = true
+            
+            let url = iTunesURL(searchText: searchBar.text!, category: segmentedControl.selectedSegmentIndex)
+            let session = URLSession.shared
+            dataTask = session.dataTask(with: url, completionHandler: { (data, response, error) in
+                //print("On the main thread? " + (Thread.current.isMainThread ? "Yes" : "No"))
+                if let error = error as NSError?, error.code == -999 {
+                    return //Seaarch was cancelled
+                } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                    if let data = data, let jsonDictionary = self.parse(json: data) {
+                        self.searchResults = self.parse(dictionary: jsonDictionary)
+                        self.searchResults.sort(by: <)
+                        DispatchQueue.main.async {
+                            self.isLoading = false
+                            self.tableView.reloadData()
+                        }
+                        return
+                    }
+                    
+                    
+                } else {
+                    print("Failure! \(response!)")
+                }
+                DispatchQueue.main.async {
+                    self.hasSearched = false
+                    self.isLoading = false
+                    self.tableView.reloadData()
+                    self.showNetworkError()
+                }
+            })
+            dataTask?.resume()
+        }
+    }
     
     func parse(json data: Data) -> [String: Any]? {
         do {
@@ -185,6 +233,11 @@ class SearchViewController: UIViewController {
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: Actions
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        performSearch()
     }
 }
 

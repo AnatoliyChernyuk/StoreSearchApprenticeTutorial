1b055dcae3c2df11bd43fc6a87db59f44ea2652c
Anatoliy Chernyuk <toliatche@gmail.com>
2017-12-04 23:53:09 +0200

    1. DetailViewController view was animated

---
+++ StoreSearchApprenticeTutorial/SlideOutAnimationController.swift
@@ -0,0 +1,29 @@
+//
+//  SlideOutAnimationController.swift
+//  StoreSearchApprenticeTutorial
+//
+//  Created by Anatoliy Chernyuk on 12/4/17.
+//  Copyright © 2017 Anatoliy Chernyuk. All rights reserved.
+//
+
+import UIKit
+
+class SlideOutAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        if let fromView = transitionContext.view(forKey: .from) {
+            let containerView = transitionContext.containerView
+            let duration = transitionDuration(using: transitionContext)
+            UIView.animate(withDuration: duration, animations: {
+                fromView.center.y -= containerView.bounds.size.height
+                fromView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            }, completion: {
+                finished in
+                transitionContext.completeTransition(finished)
+            })
+        }
+    }
+}

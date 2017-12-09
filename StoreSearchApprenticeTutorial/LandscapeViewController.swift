de241c03b182aeacd6da5dd570d9f9a60465a229
Anatoliy Chernyuk <toliatche@gmail.com>
2017-12-09 11:43:44 +0200

    1.Background image as UIColor added to landscape view 2.ScrollView become scrollable

--- StoreSearchApprenticeTutorial/LandscapeViewController.swift
+++ StoreSearchApprenticeTutorial/LandscapeViewController.swift
@@ -24,11 +24,14 @@ class LandscapeViewController: UIViewController {
         scrollView.translatesAutoresizingMaskIntoConstraints = true
         pageControl.removeConstraints(pageControl.constraints)
         pageControl.translatesAutoresizingMaskIntoConstraints = true
+        scrollView.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        scrollView.frame = view.bounds
+        pageControl.frame = CGRect(x: 0, y: view.frame.size.height - pageControl.frame.size.height, width: view.frame.size.width, height: pageControl.frame.size.height)
     }
     
 
